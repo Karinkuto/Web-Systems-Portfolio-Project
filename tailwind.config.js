c938b7b8e2b6c3a856653b18f9c72db6408ed303
Karinkuto <104971224+Karinkuto@users.noreply.github.com>
2025-05-02 16:27:44 +0300

    style: format Tailwind configuration for consistency
    - Adjust indentation and spacing in tailwind.config.js for improved readability.
    - Ensure consistent formatting across the configuration file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		fontFamily: {
-			sans: ["Poppins", "sans-serif"],
-		},
-		extend: {
-			colors: {
-				primary: "#6070FF",
-				secondary: "#2230D2",
-				accent: "#EBEBFF",
-				disabled: "#C1C7D0",
-				disabledText: "#5E6C84",
-				pressed: "#2230D2",
-				border: "#DFE1E6",
-				cardTitle: "#091E42",
-				cardBorderHover: "#A7AEFF",
-				cardSubtitleDot: "#C1C7D0",
-				cardSubtitleMain: "#344563",
-				cardSubtitleRest: "#6B778C",
-				cardContentText: "#344563",
-				appBg: "#f4f5f7",
-				heroIcon: "#505F79",
-			},
-			boxShadow: {
-				button: "0px 8px 16px 0px #4053FC3D",
-				white: "0px 8px 16px 0px rgba(255, 255, 255, 0.5)",
-				card: "0px 48px 48px 0px #252F8914",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#6070FF",
+        secondary: "#2230D2",
+        accent: "#EBEBFF",
+        disabled: "#C1C7D0",
+        disabledText: "#5E6C84",
+        pressed: "#2230D2",
+        border: "#DFE1E6",
+        cardTitle: "#091E42",
+        cardBorderHover: "#A7AEFF",
+        cardSubtitleDot: "#C1C7D0",
+        cardSubtitleMain: "#344563",
+        cardSubtitleRest: "#6B778C",
+        cardContentText: "#344563",
+        appBg: "#f4f5f7",
+        heroIcon: "#505F79",
+      },
+      boxShadow: {
+        button: "0px 8px 16px 0px #4053FC3D",
+        white: "0px 8px 16px 0px rgba(255, 255, 255, 0.5)",
+        card: "0px 48px 48px 0px #252F8914",
+      },
+    },
+  },
+  plugins: [],
 };
